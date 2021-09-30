4c0c458dbb922361dedd4c00d97d81cec3631faa
lordkartik <xbs2014@gmail.com>
2021-09-30 11:26:50 +0530

    Add files via upload

---
+++ ps_connections.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jul 05, 2021 at 10:44 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ps1752`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ps_connections`
+--
+
+CREATE TABLE `ps_connections` (
+  `id_connections` int(10) UNSIGNED NOT NULL,
+  `id_shop_group` int(11) UNSIGNED NOT NULL DEFAULT 1,
+  `id_shop` int(11) UNSIGNED NOT NULL DEFAULT 1,
+  `id_guest` int(10) UNSIGNED NOT NULL,
+  `id_page` int(10) UNSIGNED NOT NULL,
+  `ip_address` bigint(20) DEFAULT NULL,
+  `date_add` datetime NOT NULL,
+  `http_referer` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `ps_connections`
+--
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ps_connections`
+--
+ALTER TABLE `ps_connections`
+  ADD PRIMARY KEY (`id_connections`),
+  ADD KEY `id_guest` (`id_guest`),
+  ADD KEY `date_add` (`date_add`),
+  ADD KEY `id_page` (`id_page`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `ps_connections`
+--
+ALTER TABLE `ps_connections`
+  MODIFY `id_connections` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ ps_guest.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 30, 2020 at 01:35 PM
+-- Server version: 5.7.11
+-- PHP Version: 5.6.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `1733`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ps_guest`
+--
+
+CREATE TABLE `ps_guest` (
+  `id_guest` int(10) UNSIGNED NOT NULL,
+  `id_operating_system` int(10) UNSIGNED DEFAULT NULL,
+  `id_web_browser` int(10) UNSIGNED DEFAULT NULL,
+  `id_customer` int(10) UNSIGNED DEFAULT NULL,
+  `javascript` tinyint(1) DEFAULT '0',
+  `screen_resolution_x` smallint(5) UNSIGNED DEFAULT NULL,
+  `screen_resolution_y` smallint(5) UNSIGNED DEFAULT NULL,
+  `screen_color` tinyint(3) UNSIGNED DEFAULT NULL,
+  `sun_java` tinyint(1) DEFAULT NULL,
+  `adobe_flash` tinyint(1) DEFAULT NULL,
+  `adobe_director` tinyint(1) DEFAULT NULL,
+  `apple_quicktime` tinyint(1) DEFAULT NULL,
+  `real_player` tinyint(1) DEFAULT NULL,
+  `windows_media` tinyint(1) DEFAULT NULL,
+  `accept_language` varchar(8) DEFAULT NULL,
+  `mobile_theme` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ps_guest`
+--
+ALTER TABLE `ps_guest`
+  ADD PRIMARY KEY (`id_guest`),
+  ADD KEY `id_customer` (`id_customer`),
+  ADD KEY `id_operating_system` (`id_operating_system`),
+  ADD KEY `id_web_browser` (`id_web_browser`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `ps_guest`
+--
+ALTER TABLE `ps_guest`
+  MODIFY `id_guest` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
